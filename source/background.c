8650ac7ff579451ea57f0044b564d898c74cc268
keisuke-yanagi <yanagi@hep-th.phys.s.u-tokyo.ac.jp>
2017-05-09 16:59:28 +0900

    change to fermi dist without chemical potential

--- source/background.c
+++ source/background.c
@@ -966,7 +966,8 @@ int background_ncdm_distribution(
     /*    FERMI-DIRAC INCLUDING CHEMICAL POTENTIALS   */
     /**************************************************/
 
-    *f0 = 1.0/pow(2*_PI_,3)*(1./(exp(q-ksi)+1.) +1./(exp(q+ksi)+1.));
+    /* *f0 = 1.0/pow(2*_PI_,3)*(1./(exp(q-ksi)+1.) +1./(exp(q+ksi)+1.)); */
+    *f0 = 1./(exp(q)+1.);
    
     
     /**************************************************/
